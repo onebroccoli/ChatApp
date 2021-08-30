476fc58a6e36d2b872dcbce8745d68f9f0ee89cc
onebroccoli <three.broccoli@gmail.com>
2021-08-30 12:00:13 -0700

    create project

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Sophia Zhu on 8/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
