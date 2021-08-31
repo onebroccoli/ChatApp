c6dbaa5c34010dfa73148419d89773ce6a865128
onebroccoli <three.broccoli@gmail.com>
2021-08-31 10:26:09 -0700

    login signup animations

--- ChatApp/ChatApp/LoginViewController.swift
+++ ChatApp/ChatApp/LoginViewController.swift
@@ -43,11 +43,17 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var repeatPasswordLineView: UIView!
     
     
+    // MARK: - Vars
+    var isLogin = true
+    
+    
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUIfor(login: true)
+        setupTextFieldDelegates()
     }
     
     // MARK: - IBActions
@@ -60,9 +66,61 @@ class LoginViewController: UIViewController {
     
     @IBAction func resendEmailButtonPressed(_ sender: Any) {
     }
-    @IBAction func signUpButtonPressed(_ sender: Any) {
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        updateUIfor(login: sender.titleLabel?.text == "Login")
+        isLogin.toggle()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupTextFieldDelegates() {
+        emailTextField.addTarget(self, action: #selector(textFieldChange(_ :)), for: .editingChanged) //whenever is typed, action changed
+        
+        passwordTextField.addTarget(self, action: #selector(textFieldChange(_ :)), for: .editingChanged) //whenever is typed, action changed
+        
+        repeatPasswordTextField.addTarget(self, action: #selector(textFieldChange(_ :)), for: .editingChanged) //whenever is typed, action changed
+        
+    }
+    
+    @objc func textFieldChange(_ textField: UITextField) {
+        
+        print("changing text field")
+        updatePlaceholderLabels(textField: textField)
     }
 
+    // MARK: - Animation
+    
+    private func updateUIfor(login: Bool) {
+        
+        loginButtonOutlet.setImage(UIImage(named: login ? "loginBtn" : "registerBtn"), for: .normal)// based on login flag choose register or login
+        
+        signUpButtonOutlet.setTitle(login ? "Signup" : "Login", for: .normal)
+        
+        signUpLabel.text = login ? "Don't have an account?" : "Have an account?"
+        
+        UIView.animate(withDuration: 0.5) {
+            self.repeatPasswordTextField.isHidden = login
+            self.repeatPasswordLabel.isHidden = login
+            self.repeatPasswordLineView.isHidden = login
+        }
+        
+    }
+    
+    
+    
+    private func updatePlaceholderLabels(textField: UITextField) {
+        
+        switch textField {
+        case emailTextField:
+            emailLabelOutlet.text = textField.hasText ? "Email" : ""
+        case passwordTextField:
+            passwordLabelOutlet.text = textField.hasText ? "Password" : ""
 
+        default:
+            repeatPasswordLabel.text = textField.hasText ? "Repeat Password" : ""
+        
+        }
+    }
+    
 }
 
