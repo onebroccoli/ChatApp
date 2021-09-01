c8d6e0a8d98289f90eb1fc4c0d237e17b9a88e80
onebroccoli <three.broccoli@gmail.com>
2021-09-01 00:28:02 -0700

    show notifications for data input

--- ChatApp/ChatApp/LoginViewController.swift
+++ ChatApp/ChatApp/LoginViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import ProgressHUD
 
 class LoginViewController: UIViewController {
 
@@ -54,17 +55,39 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         updateUIfor(login: true)
         setupTextFieldDelegates()
+        setupBackgroundTap()
     }
     
     // MARK: - IBActions
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        //login or resgister
+        if isDataInputedFor(type: isLogin ? "login" : "register") {
+            //login or register
+            print("have data login/reg")
+            
+        } else {
+            ProgressHUD.showFailed("All fields are required")
+        }
     }
     
     @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
+        if isDataInputedFor(type: "password"){
+            //reset password
+            print("have data for forgot password")
+            
+        } else {
+            ProgressHUD.showFailed("Email is required.")
+        }
     }
     
     @IBAction func resendEmailButtonPressed(_ sender: Any) {
+        if isDataInputedFor(type: "password"){
+            //resend verification email
+            print("have data for resend email")
+        } else {
+            ProgressHUD.showFailed("Email is required")
+        }
     }
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
         updateUIfor(login: sender.titleLabel?.text == "Login")
@@ -87,12 +110,25 @@ class LoginViewController: UIViewController {
         print("changing text field")
         updatePlaceholderLabels(textField: textField)
     }
-
+    
+    // whenever user type on the screen, need to show keyboard
+    private func setupBackgroundTap() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func backgroundTap() {
+//        print ("background tap")
+        view.endEditing(false)
+    }
+    
+    
     // MARK: - Animation
     
     private func updateUIfor(login: Bool) {
         
-        loginButtonOutlet.setImage(UIImage(named: login ? "loginBtn" : "registerBtn"), for: .normal)// based on login flag choose register or login
+        loginButtonOutlet.setTitle(login ? "Login" : "Register", for: .normal);
+//      
         
         signUpButtonOutlet.setTitle(login ? "Signup" : "Login", for: .normal)
         
@@ -119,6 +155,20 @@ class LoginViewController: UIViewController {
         default:
             repeatPasswordLabel.text = textField.hasText ? "Repeat Password" : ""
         
+        }
+    }
+    
+    // MARK: - Helpers
+    private func isDataInputedFor(type: String) -> Bool {
+        
+        switch type {
+        case "login":
+            return emailTextField.text != "" && passwordTextField.text != ""
+        case "registration":
+            return emailTextField.text != "" && passwordTextField.text != "" && repeatPasswordTextField.text != ""
+        default:
+            return emailTextField.text != ""
+            
         }
     }
     
