83b409a3f6bb81f788262999692ea77e737d8a13
onebroccoli <three.broccoli@gmail.com>
2021-09-06 15:04:55 -0700

    FCollectionReference

---
+++ ChatApp/ChatApp/FirebaseHelpers/FCollectionReference.swift
@@ -0,0 +1,20 @@
+//
+//  FCollectionReference.swift
+//  ChatApp
+//
+//  Created by Sophia Zhu on 9/1/21.
+//
+
+import Foundation
+import FirebaseFirestore
+enum FCollectionReference: String {
+    case User
+    case Recent
+}
+
+
+func FirebaseReference(_ collectionReference: FCollectionReference) -> CollectionReference {
+    
+    return Firestore.firestore().collection(collectionReference.rawValue)
+    
+}
