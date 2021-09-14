7f13dc54739213f04a8c233136649320dc32923e
onebroccoli <three.broccoli@gmail.com>
2021-09-14 11:25:34 -0700

    Create StatusTableViewController.swift

---
+++ ChatApp/ChatApp/StatusTableViewController.swift
@@ -0,0 +1,81 @@
+//
+//  StatusTableViewController.swift
+//  ChatApp
+//
+//  Created by Sophia Zhu on 9/13/21.
+//
+
+import UIKit
+
+class StatusTableViewController: UITableViewController {
+    // MARK: - Vars
+    var allStatuses: [String] = []
+    
+    // MARK: - ViewLifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        loadUserStatus()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allStatuses.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let status = allStatuses[indexPath.row]
+        cell.textLabel?.text = status
+        
+        //put checkmark when select certain status
+        cell.accessoryType = User.currentUser?.status == status ? .checkmark :.none
+        
+        
+        return cell
+            
+    }
+    
+    // MARK: - Tableview delegates
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        updateCellCheck(indexPath)
+        tableView.reloadData()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor(named: "tableViewBackgroundColor")
+        return headerView
+    }
+    
+    //MARK: -Loading Status
+    private func loadUserStatus() {
+        print("loaded status")
+        allStatuses = userDefaults.object(forKey: kSTATUS) as! [String]
+        tableView.reloadData()
+    }
+    
+    private func updateCellCheck(_ indexPath: IndexPath) {
+        if var user = User.currentUser {
+            user.status = allStatuses[indexPath.row]
+            saveUserLocally(user)
+            FirebaseUserListener.shared.saveUserToFirestore(user)
+        }
+        
+    }
+
+    
+}
