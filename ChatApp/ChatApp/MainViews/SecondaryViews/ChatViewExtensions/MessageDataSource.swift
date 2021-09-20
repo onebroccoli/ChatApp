a7dc7ca00a4ddbe0c1564b3194d7d1baae55e05e
onebroccoli <three.broccoli@gmail.com>
2021-09-20 14:37:44 -0700

    Create MessageDataSource.swift

---
+++ ChatApp/ChatApp/MainViews/SecondaryViews/ChatViewExtensions/MessageDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  MessageDataSource.swift
+//  ChatApp
+//
+//  Created by Sophia Zhu on 9/20/21.
+//
+
+import Foundation
+import MessageKit
+
+extension ChatViewController: MessagesDataSource {
+    func currentSender() -> SenderType {
+        return currentUser
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return mkMessages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return mkMessages.count
+    }
+    
+    
+}
