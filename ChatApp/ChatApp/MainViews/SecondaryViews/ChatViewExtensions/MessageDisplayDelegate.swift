d50205be03585d9f8dfe372fee2282efb688136a
onebroccoli <three.broccoli@gmail.com>
2021-09-20 14:37:47 -0700

    Create MessageDisplayDelegate.swift

---
+++ ChatApp/ChatApp/MainViews/SecondaryViews/ChatViewExtensions/MessageDisplayDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  MessageDisplayDelegate.swift
+//  ChatApp
+//
+//  Created by Sophia Zhu on 9/20/21.
+//
+
+import Foundation
+import MessageKit
+
+extension ChatViewController : MessagesDisplayDelegate {
+    
+    
+}
