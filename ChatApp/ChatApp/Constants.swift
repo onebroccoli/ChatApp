6a4854d1554a1c233130a9252972711dcbce178a
onebroccoli <three.broccoli@gmail.com>
2021-09-21 16:01:16 -0700

    Update Constants.swift

--- ChatApp/ChatApp/Constants.swift
+++ ChatApp/ChatApp/Constants.swift
@@ -14,3 +14,17 @@ public let kFILEREFERENCE = "gs://chapapp-660ad.appspot.com"
 public let kCURRENTUSER = "currentUser"
 public let kSTATUS = "status"
 public let kFIRSTRUN = "firstRUN"
+public let kCHATROOMID = "chatRoomID"
+public let kSENDERID = "senderId"
+
+public let kSENT = "Sent"
+public let kREAD = "Read"
+
+public let kTEXT = "text"
+public let kPHOTO = "photo"
+public let kVIDEO = "video"
+public let kAUDIO = "audio"
+public let kLOCATION = "location"
+
+
+
