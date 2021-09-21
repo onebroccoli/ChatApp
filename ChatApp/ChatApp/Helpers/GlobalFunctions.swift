d9a5e0321f98b6bfff3bd5ef06ed07bf8b313dc8
onebroccoli <three.broccoli@gmail.com>
2021-09-21 16:01:23 -0700

    Update GlobalFunctions.swift

--- ChatApp/ChatApp/Helpers/GlobalFunctions.swift
+++ ChatApp/ChatApp/Helpers/GlobalFunctions.swift
@@ -18,8 +18,30 @@ import Foundation
 // last OzPBJ83sTRaVatJxgbkAl7otolp2
 
 func fileNameFrom(fileUrl: String) -> String {
+
     return ((fileUrl.components(separatedBy: "_").last)!.components(separatedBy: "?").first!).components(separatedBy: ".").first!
-    
-    
-    
+
+}
+
+
+// show in the chat page, show how long the msg was received. just now/ minutes/ hours ago
+func timeElapsed(_ date: Date) -> String {
+    let seconds = Date().timeIntervalSince(date)
+    var elapsed = ""
+    if seconds < 60 {
+        elapsed = "Just now"
+    } else if seconds < 60*60 {
+        let minutes = Int(seconds / 60)
+        let minText = minutes > 1 ? "mins" : "min"
+        elapsed = " \(minutes) \(minText)"
+        
+    } else if seconds < 24 * 60 * 60 {
+        let hours = Int(seconds / (60 * 60))
+        let hourText = hours > 1 ? "hours" : "hour"
+
+        elapsed = "\(hours) \(hourText)"
+    } else {
+        elapsed = date.longDate()
+    }
+    return elapsed
 }
