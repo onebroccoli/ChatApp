6688f3bfb8485515dbd3a897250fde71eabdb4a2
onebroccoli <three.broccoli@gmail.com>
2021-09-21 16:01:36 -0700

    Create MessageCellDelegate.swift

---
+++ ChatApp/ChatApp/MainViews/SecondaryViews/ChatViewExtensions/MessageCellDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  MessageCellDelegate.swift
+//  ChatApp
+//
+//  Created by Sophia Zhu on 9/20/21.
+//
+
+import Foundation
+import MessageKit
+
+extension ChatViewController : MessageCellDelegate {
+    
+    
+}
