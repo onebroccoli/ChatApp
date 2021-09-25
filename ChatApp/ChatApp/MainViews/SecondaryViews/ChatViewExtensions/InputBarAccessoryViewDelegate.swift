2cdb597368106db06177bf9c08d52d79a4584504
onebroccoli <three.broccoli@gmail.com>
2021-09-24 21:44:34 -0700

    Update InputBarAccessoryViewDelegate.swift

--- ChatApp/ChatApp/MainViews/SecondaryViews/ChatViewExtensions/InputBarAccessoryViewDelegate.swift
+++ ChatApp/ChatApp/MainViews/SecondaryViews/ChatViewExtensions/InputBarAccessoryViewDelegate.swift
@@ -10,14 +10,20 @@ import InputBarAccessoryView
 
 extension ChatViewController : InputBarAccessoryViewDelegate {
     func inputBar(_ inputBar: InputBarAccessoryView, textViewTextDidChangeTo text: String) {
-        print("typing...")
+        if text != "" {
+//            print("typing...")
+            typingIndicatorUpdate()
+
+        }
+        updateMicButtonStatus(show: text == "")
     }
     
     func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
         for component in inputBar.inputTextView.components {
             if let text = component as? String {
                 
-                print("send message with text ", text)
+//                print("send message with text ", text)
+                messageSend(text: text, photo: nil, video: nil, audio: nil, location: nil)
                 
             }
         }
