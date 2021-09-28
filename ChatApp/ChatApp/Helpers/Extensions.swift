3052d8de554432985236898924876920d499e17b
onebroccoli <three.broccoli@gmail.com>
2021-09-28 16:27:42 -0700

    Update Extensions.swift

--- ChatApp/ChatApp/Helpers/Extensions.swift
+++ ChatApp/ChatApp/Helpers/Extensions.swift
@@ -47,4 +47,12 @@ extension Date {
         dateFormatter.dateFormat = "ddMMyyyyHHmmss" //for save to firebase
         return dateFormatter.string(from: self)
     }
+    
+    func interval(ofComponent comp: Calendar.Component, from date: Date) -> Float {
+        let currentCalendar = Calendar.current
+        guard let start = currentCalendar.ordinality(of: comp, in: .era, for: date) else { return 0 }
+        guard let end =   currentCalendar.ordinality(of: comp, in: .era, for: self) else { return 0 }
+        
+        return Float(start - end)
+    }
 }
