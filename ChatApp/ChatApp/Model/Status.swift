38cb6fc94abe524410d53157d90fcb46d0317a4d
onebroccoli <three.broccoli@gmail.com>
2022-05-09 13:58:45 -0700

    Update Status.swift

--- ChatApp/ChatApp/Model/Status.swift
+++ ChatApp/ChatApp/Model/Status.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Status: String {
+enum Status: String, CaseIterable {
     
     case Available = "Available"
     case Busy = "Busy"
@@ -20,35 +20,52 @@ enum Status: String {
     case AtTheGym = "At the Gym"
     case Sleeping = "Sleeping"
     case UrgentCallsOnly = "Urgent Calls Only"
-    
-    static var array: [Status] {
-        var a: [Status] = []
-        
-        switch Status.Available {
-        case .Available:
-            a.append(.Available); fallthrough
-        case .Busy:
-            a.append(.Busy); fallthrough
-        case .AtSchool:
-            a.append(.AtSchool); fallthrough
-        case .AtTheMovies:
-            a.append(.AtTheMovies); fallthrough
-        case .AtWork:
-            a.append(.AtWork); fallthrough
-        case .BatteryAboutToDie:
-            a.append(.BatteryAboutToDie); fallthrough
-        case .CantTalk:
-            a.append(.CantTalk); fallthrough
-        case .InAMeeting:
-            a.append(.InAMeeting); fallthrough
-        case .AtTheGym:
-            a.append(.AtTheGym); fallthrough
-        case .Sleeping:
-            a.append(.Sleeping); fallthrough
-        case .UrgentCallsOnly:
-            a.append(.UrgentCallsOnly)
-       print(a)
-        return a
-        }
-    }
 }
+
+
+
+//enum Status: String {
+//
+//    case Available = "Available"
+//    case Busy = "Busy"
+//    case AtSchool = "AtSchool"
+//    case AtTheMovies = "At The Movies"
+//    case AtWork = "At Work"
+//    case BatteryAboutToDie = "Battery About To Die"
+//    case CantTalk = "Can't Talk"
+//    case InAMeeting = "In a Meeting"
+//    case AtTheGym = "At the Gym"
+//    case Sleeping = "Sleeping"
+//    case UrgentCallsOnly = "Urgent Calls Only"
+//
+//    static var array: [Status] {
+//        var a: [Status] = []
+//
+//        switch Status.Available {
+//        case .Available:
+//            a.append(.Available); fallthrough
+//        case .Busy:
+//            a.append(.Busy); fallthrough
+//        case .AtSchool:
+//            a.append(.AtSchool); fallthrough
+//        case .AtTheMovies:
+//            a.append(.AtTheMovies); fallthrough
+//        case .AtWork:
+//            a.append(.AtWork); fallthrough
+//        case .BatteryAboutToDie:
+//            a.append(.BatteryAboutToDie); fallthrough
+//        case .CantTalk:
+//            a.append(.CantTalk); fallthrough
+//        case .InAMeeting:
+//            a.append(.InAMeeting); fallthrough
+//        case .AtTheGym:
+//            a.append(.AtTheGym); fallthrough
+//        case .Sleeping:
+//            a.append(.Sleeping); fallthrough
+//        case .UrgentCallsOnly:
+//            a.append(.UrgentCallsOnly)
+//       print(a)
+//        return a
+//        }
+//    }
+//}
