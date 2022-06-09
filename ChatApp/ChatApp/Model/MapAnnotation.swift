462aabe3b3869ee0b1a7d3208d05cb2c9e7f9238
onebroccoli <three.broccoli@gmail.com>
2022-06-08 19:46:21 -0700

    Update MapAnnotation.swift

--- ChatApp/ChatApp/Model/MapAnnotation.swift
+++ ChatApp/ChatApp/Model/MapAnnotation.swift
@@ -13,6 +13,7 @@ class MapAnnotation: NSObject, MKAnnotation {
     let title: String?
     let coordinate: CLLocationCoordinate2D
     
+    
     init(title: String?, coordinate: CLLocationCoordinate2D) {
         self.title = title
         self.coordinate = coordinate
